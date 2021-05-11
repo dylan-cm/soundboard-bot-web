1ca2a99495c207165ba095b342d83faa62f8a9bc
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-05-11 14:12:26 -0700

    sounboard bot splash page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './soundlogo.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Soundboard Bot</h2>
+        <h5>
+          A Discord Bot that play sound effects right in your voice channels.
+        </h5>
+        <h6>Try typing <code>!joke</code> in a text channel when you are joined in a voice channel.</h6>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://discord.com/oauth2/authorize?client_id=840782180455809025&scope=bot"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Add to your Discord!
         </a>
       </header>
     </div>
